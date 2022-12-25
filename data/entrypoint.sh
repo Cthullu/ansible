2665456dc031922699f3c16eef3135b3c0f49f26
Daniel Kuß <d.kuss86@googlemail.com>
2022-12-26 00:38:30 +0100

    Use system cert store
    The system certificate store should be used, if custom certificates are added

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -13,6 +13,9 @@ fi
 if [[ $(ls /tmp/certs/ | wc -w) -gt 0 ]]; then
   sudo cp /tmp/certs/*.crt /usr/local/share/ca-certificates/
   sudo update-ca-certificates
+
+  # Also, tell python to use the system cert store, instead of his own
+  export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 fi
 
 /usr/bin/env sh -l
