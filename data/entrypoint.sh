6bfd51c466a4841577d0e98cb54be38cc4f0e393
Daniel Kuß <d.kuss86@googlemail.com>
2022-12-25 23:39:18 +0100

    Rename folder

--- data/entrypoint.sh
+++ data/entrypoint.sh
@@ -2,10 +2,15 @@
 
 ACTIVATE_ARA_ENV="${ACTIVATE_ARA_ENV:=false}"
 
+# Add ARA plugin paths to environment
 if [[ ${ACTIVATE_ARA_ENV} == "true" ]]; then
   export ANSIBLE_ACTION_PLUGINS="$(python3 -m ara.setup.action_plugins)"
   export ANSIBLE_CALLBACK_PLUGINS="$(python3 -m ara.setup.callback_plugins)"
   export ANSIBLE_LOOKUP_PLUGINS="$(python3 -m ara.setup.lookup_plugins)"
 fi
 
-/usr/bin/env sh
+# Add custom certificates to local CA-store
+if [[ $(ls /tmp/certs/ | wc -w) -gt 0 ]]; then
+  sudo cp /tmp/certs/*.crt /usr/local/share/ca-certificates/
+  sudo update-ca-certificates
+fi
