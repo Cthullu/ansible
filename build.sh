cfd8deded24382ce0342efed2041689fd1379a1a
Daniel Kuß <d.kuss86@googlemail.com>
2023-06-25 17:40:17 +0200

    Change build script settings

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ podman build \
 --force-rm \
 --no-cache \
 --platform linux/amd64 \
---quiet \
 --rm \
---tag quay.io/cthullu/bind9:${VERSION} \
+--tag quay.io/cthullu/ansible:${VERSION} \
 --file Dockerfile .
